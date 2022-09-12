f00fbfa9316bca1433177ebf992cf25e346ca508
AbhinashJha342 <abhinashjha09@gmail.com>
2022-09-12 13:35:13 -0500

    Java school- updating query for find by name

--- src/main/java/com/example/employee/persistence/EmployeeRepository.java
+++ src/main/java/com/example/employee/persistence/EmployeeRepository.java
@@ -25,6 +25,12 @@ public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
     Optional<Employee> findEmployeesByEmployeeIdAndDeletedIsFalse(UUID employeeId);
 
+    @Query(value = "select employee0_.id, employee0_.date_of_birth, \n" +
+            "employee0_.deleted, employee0_.employee_id, employee0_.gender \n" +
+            ",employee0_.name_id, name1_.first, name1_.last, employee0_.phone \n" +
+            "from employee employee0_ left outer join employee_names name1_ on employee0_.name_id=name1_.id \n" +
+            "where name1_.first ilike concat('%', :first, '%') OR name1_.last ilike concat('%', :last, '%') \n" +
+            "and employee0_.deleted=false", nativeQuery = true)
     Optional<List<Employee>> findEmployeesByName_FirstContainingIgnoreCaseAndName_LastContainingIgnoreCaseAndDeletedIsFalse(@Param("first") String first, @Param("last") String last);
 
     @Query(value = "select emp from Employee emp Where " +
