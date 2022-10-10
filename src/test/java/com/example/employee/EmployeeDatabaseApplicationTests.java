b2eec92431eb3d8ee1369fd0736f094bc62d9816
AbhinashJha342 <abhinashjha09@gmail.com>
2022-10-10 16:51:46 -0500

    Java school- changes to use liquibase and h2 database for tests

--- src/test/java/com/example/employee/EmployeeDatabaseApplicationTests.java
+++ src/test/java/com/example/employee/EmployeeDatabaseApplicationTests.java
@@ -1,13 +1,76 @@
 package com.example.employee;
 
+import com.example.employee.util.EmployeeProfileUtil;
+import com.example.employee.web.AbstractControllerIntegrationTest;
+import com.example.employee.web.EmployeeAdminController;
+import com.example.employee.web.UserController;
+import com.example.employee.web.schema.EmployeeDetailsResponseDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.tomakehurst.wiremock.common.Json;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 
 @SpringBootTest
-class EmployeeDatabaseApplicationTests {
+@AutoConfigureMockMvc
+class EmployeeDatabaseApplicationTests extends AbstractControllerIntegrationTest{
+
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup(){
+        this.mockMvc = webAppContextSetup(this.wac).build();
+    }
 
     @Test
-    void contextLoads() {
+    void createEmployeeTest() throws Exception {
+        this.mockMvc.perform(post("/admin/employees")
+                        .accept(EmployeeProfileUtil.MEDIA_TYPE_JSON_UTF8)
+                        .contentType(EmployeeProfileUtil.MEDIA_TYPE_JSON_UTF8)
+                        .content(new ObjectMapper().writeValueAsString(EmployeeProfileUtil.createEmployeeRequest())))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void getEmployeeByEmployeeId() throws Exception {
+        String response = this.mockMvc.perform(post("/admin/employees")
+                        .accept(EmployeeProfileUtil.MEDIA_TYPE_JSON_UTF8)
+                        .contentType(EmployeeProfileUtil.MEDIA_TYPE_JSON_UTF8)
+                        .content(new ObjectMapper().writeValueAsString(EmployeeProfileUtil.createEmployeeRequest())))
+                .andReturn()
+                .getResponse().getContentAsString();
+
+        EmployeeDetailsResponseDTO resultDto = new ObjectMapper().readValue(response, EmployeeDetailsResponseDTO.class);
+
+        this.mockMvc.perform(get("/admin/employees")
+                .accept(EmployeeProfileUtil.MEDIA_TYPE_JSON_UTF8)
+                .contentType(EmployeeProfileUtil.MEDIA_TYPE_JSON_UTF8)
+                .header("Employee-id", resultDto.getEmployeeId()))
+                .andExpect(status().is2xxSuccessful());
     }
 
 }
--- src/test/java/com/example/employee/web/AbstractControllerIntegrationTest.java
+++ src/test/java/com/example/employee/web/AbstractControllerIntegrationTest.java
@@ -1,4 +1,27 @@
 package com.example.employee.web;
 
+import com.example.employee.container.TestPostgresContainer;
+import org.junit.ClassRule;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+//@ActiveProfiles({"test"})
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@ContextConfiguration(classes = AppConfig.class)
+@ExtendWith(SpringExtension.class)
+//@DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
+@TestPropertySource(value = {
+        "classpath:application-test.properties"
+})
 public abstract class AbstractControllerIntegrationTest {
+
+    @ClassRule
+    public static TestPostgresContainer postgres = TestPostgresContainer
+            .getInstance();
 }
