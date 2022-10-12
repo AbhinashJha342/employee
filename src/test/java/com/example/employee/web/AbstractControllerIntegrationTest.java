7abd653992e4ad88662f16aa352256a6d45462c8
AbhinashJha342 <abhinashjha09@gmail.com>
2022-10-12 17:21:33 -0500

    Java school- setup test containers

--- src/test/java/com/example/employee/web/AbstractControllerIntegrationTest.java
+++ src/test/java/com/example/employee/web/AbstractControllerIntegrationTest.java
@@ -1,7 +1,5 @@
 package com.example.employee.web;
 
-import com.example.employee.container.TestPostgresContainer;
-import org.junit.ClassRule;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.test.context.ActiveProfiles;
@@ -19,8 +17,4 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
         "classpath:application-test.properties"
 })
 public abstract class AbstractControllerIntegrationTest {
-
-    @ClassRule
-    public static TestPostgresContainer postgres = TestPostgresContainer
-            .getInstance();
 }
