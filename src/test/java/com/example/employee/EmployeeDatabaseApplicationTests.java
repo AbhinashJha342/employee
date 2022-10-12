0522acc5e260a51577d1dedc5ff0f01f07a3833e
AbhinashJha342 <abhinashjha09@gmail.com>
2022-10-12 17:32:23 -0500

    Java school- successful tests with test containers

--- src/test/java/com/example/employee/EmployeeDatabaseApplicationTests.java
+++ src/test/java/com/example/employee/EmployeeDatabaseApplicationTests.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.context.WebApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
